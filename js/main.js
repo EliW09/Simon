12a61afde1bab5d777e058c81a4dd8fd84903afb
EliW09 <eli@dogdigger.com>
2018-10-10 16:51:48 -0400

    started game logic

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var colors = ["green", "blue", "red", "yellow"];
+var startColors = [];
+var simonColors = [];
+var userColors = [];
+
+colors.sort(() => Math.random() - 0.5);
+startColors = colors[0];
+simonColors = startColors;
+
+function startSimon {
+  if (simonColors === "blue") {
+    
+  }
+}
